fbfbbd1d5d802d48e4637e8d66adc578c9a985e7
Hassir Lastre Sierra <84827499+hlastre@users.noreply.github.com>
2022-10-04 20:39:59 -0500

    Se corrigió el tipo de variable fecha.

--- src/main/java/com/hels/Techgyment/Entities/Movement.java
+++ src/main/java/com/hels/Techgyment/Entities/Movement.java
@@ -3,7 +3,7 @@ package com.hels.Techgyment.Entities;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.sql.Date;
+import java.time.LocalDate;
 
 @Data //Crea los getters and setters de cada uno de los atributos; también un constructor vacío.
 @Entity
@@ -28,16 +28,16 @@ public class Movement {
     private String enterprise;
 
     @Column(name = "created_at")
-    private Date created_at;
+    private LocalDate created_at;
 
     @Column(name = "updated_at")
-    private Date updated_at;
+    private LocalDate updated_at;
 
     public Movement() {
 
     }
 
-    public Movement(Long id, String concept, double amount, String employee, String enterprise, Date created_at, Date updated_at) {
+    public Movement(Long id, String concept, double amount, String employee, String enterprise, LocalDate created_at, LocalDate updated_at) {
         this.id = id;
         this.concept = concept;
         this.amount = amount;
